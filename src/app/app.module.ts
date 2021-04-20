fda2026c426a8082c632196bb5ad8c39385d018d
angel1795 <mpangel1995@outlook.es>
2021-04-20 22:50:39 +0200

    Nav, carousel, fixes updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,25 @@ import { AppComponent } from './app.component';
 
 import { CardsComponent } from './cards/cards.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeMenuComponent } from './home-menu/home-menu.component';
+import { CarouselSearcherComponent } from './carousel-searcher/carousel-searcher.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
     AppComponent,
       CardsComponent,
       CardsComponent,
-      FooterComponent
+      FooterComponent,
+      HomeMenuComponent,
+      CarouselSearcherComponent
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
