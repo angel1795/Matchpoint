3c33c794736929ed3d9101111ee8e8a76ded35a1
angel1795 <mpangel1995@outlook.es>
2021-04-27 23:28:53 +0200

    Upgrade home login & register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { CardsComponent } from './cards/cards.component';
+import { CardsComponent } from './landing/cards/cards.component';
 import { FooterComponent } from './footer/footer.component';
-import { HomeMenuComponent } from './home-menu/home-menu.component';
-import { CarouselSearcherComponent } from './carousel-searcher/carousel-searcher.component';
+import { HomeMenuComponent } from './landing/home-menu/home-menu.component';
+import { CarouselSearcherComponent } from './landing/carousel-searcher/carousel-searcher.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import {MatInputModule} from '@angular/material/input';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import { LandingComponent } from './landing/landing.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-      CardsComponent,
+
       CardsComponent,
       FooterComponent,
       HomeMenuComponent,
-      CarouselSearcherComponent
+      CarouselSearcherComponent,
+      LoginComponent,
+      RegisterComponent,
+      LandingComponent,
+
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NoopAnimationsModule,
-    MatInputModule
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatIconModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
